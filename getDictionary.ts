d9aa1961dffa3d51342d16369df13d59872d774d
marvin <sea-son@foxmail.com>
2024-11-08 10:37:25 +0800

    📦 NEW: base case for i18n-app next14 src

--- getDictionary.ts
+++ getDictionary.ts
@@ -5,7 +5,6 @@ import type { Locale } from "./i18n-config";
 // We also get the default import for cleaner types
 const dictionaries = {
   en: () => import("./dictionaries/en.json").then((module) => module.default),
-  // de: () => import("./dictionaries/de.json").then((module) => module.default),
   zh: () => import("./dictionaries/zh.json").then((module) => module.default),
 };
 
--- src/app/[lang]/components/counter.tsx
+++ src/app/[lang]/components/counter.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { type getDictionary } from "../../../getDictionary";
+import { type getDictionary } from "../../../../getDictionary";
 
 export default function Counter({
   dictionary,
