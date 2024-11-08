a2ef6e8bbc7396be4ba8c320fad93c065f176d28
marvin <sea-son@foxmail.com>
2024-11-08 14:52:46 +0800

    📦 NEW: request header Accept-Language

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse, NextRequest } from "next/server";
 import { getUserLocale } from "./service/locale";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  response.headers.set("Accept-Language", await getUserLocale());
-  return response;
+  request.headers.set("Accept-Language", await getUserLocale());
+  return NextResponse.next();
 }
